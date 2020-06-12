180cd7c6d67a70981e9a682eb3215064d68f6d2f
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2020-06-12 12:50:03 +0530

    Done with setup

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, 
+  FlatList, Image, ActivityIndicator} from 'react-native';
+import {Card, CardItem} from 'native-base';
 
 export default function App() {
   return (
